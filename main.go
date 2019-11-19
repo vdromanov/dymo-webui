a289f86bc5db1d82b5d234c16b1d089526997356
vdromanov <vdromanov@yahoo.com>
2019-11-19 21:59:43 +0300

    added command line flags to specify exec_script and port to run on

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
 )
 
@@ -12,14 +14,19 @@ func runPrinting(w http.ResponseWriter, r *http.Request) {
 	} else {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
-
 }
 
 func main() {
-	ScriptLocation = "labels.py"
+	var port int
+
+	flag.StringVar(&ScriptLocation, "exec", "labels.py", "Specify printing python script")
+	flag.IntVar(&port, "port", 4080, "Port, to run on")
+	flag.Parse()
+
 	server := http.Server{
-		Addr: ":4080",
+		Addr: fmt.Sprintf(":%d", port),
 	}
+
 	http.Handle("/", http.FileServer(AssetFile()))
 	http.HandleFunc("/print", runPrinting)
 	server.ListenAndServe()
